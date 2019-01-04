1d38962758bbd1a24f30272806ca930c916bf8a0
Rafael Varago <rafael.varago@egym.de>
2019-01-04 13:23:37 +0100

    Add new controller: integral
    That outputs a control signal proportional to the discrete integral
    of the error along the time.

---
+++ test/controller_integral_test.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include "cpppid/controllers/integral.hpp"
+
+using namespace cpppid::controllers;
+
+TEST(CppPIDintegral, shouldReturnProportionalToTheIntegralOfTheError) {
+    auto ctrl = integral<int, double, int>{2, 3.0};
+
+    EXPECT_NEAR(ctrl(0), 0.000, 0.001);
+    EXPECT_NEAR(ctrl(1), 6.000, 0.001);
+    EXPECT_NEAR(ctrl(4), 30.000, 0.001);
+    EXPECT_NEAR(ctrl(14), 114.000, 0.001);
+    EXPECT_NEAR(ctrl(3), 132.000, 0.001);
+}
