7acf87a356c2535183e7fe78608f6b4b94d8e997
Rafael Varago <rafael.varago@egym.de>
2019-01-04 12:26:50 +0100

    Add new controller: proportional
    That outputs a control signal proportional to the current error.
    
    Also removed the now unnecessary file pid.hpp, because the library will
    allow to compose controller based on unit blocks.

---
+++ include/cpppid/controllers/proportional.hpp
@@ -0,0 +1,25 @@
+#ifndef PROPORTIONAL_HPP
+#define PROPORTIONAL_HPP
+
+#include <utility>
+
+namespace cpppid {
+    namespace controllers {
+
+        template <typename Kp>
+        class proportional {
+            public:
+                proportional(Kp factor) : m_factor{std::move(factor)} {}
+
+                template <typename Error>
+                auto operator()(Error const& current_error) {
+                    return m_factor * current_error;
+                }
+
+            private:
+                Kp const m_factor;
+        };
+    }
+}
+
+#endif
--- test/cpppid_test.cpp
+++ test/cpppid_test.cpp
@@ -1,9 +1,4 @@
 #include <gtest/gtest.h>
-#include "cpppid/pid.hpp"
-
-TEST(pid, pid) {
-    //ASSERT_DOUBLE_EQ(0.0, cpppid::pid());
-}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
