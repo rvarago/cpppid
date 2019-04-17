5ea9188ce5f65209dac207cd2c3c83161ba5cfdc
Rafael Varago <rafael.varago@egym.de>
2019-04-17 16:04:38 +0200

    refactor: Use C++ 17 nested namespaces

--- include/cpppid/controllers/derivative.hpp
+++ include/cpppid/controllers/derivative.hpp
@@ -3,30 +3,28 @@
 
 #include <utility>
 
-namespace cpppid {
-    namespace controllers {
+namespace cpppid::controllers {
 
-        template <typename Kd = double, typename TimeInterval = int, typename PreviousError = double>
-        class derivative {
-            public:
-                explicit derivative(Kd factor, TimeInterval interval, PreviousError initial_error = PreviousError{})
-                    : m_factor{std::move(factor)},
-                      m_interval{std::move(interval)},
-                      m_previousError{std::move(initial_error)} {}
+    template <typename Kd = double, typename TimeInterval = int, typename PreviousError = double>
+    class derivative {
+        public:
+            explicit derivative(Kd factor, TimeInterval interval, PreviousError initial_error = PreviousError{})
+                : m_factor{std::move(factor)},
+                    m_interval{std::move(interval)},
+                    m_previousError{std::move(initial_error)} {}
 
-                template <typename Error>
-                auto operator()(Error const& current_error) {
-                    auto const delta_error = current_error - m_previousError;
-                    m_previousError = current_error;
-                    return m_factor * delta_error / m_interval;
-                }
+            template <typename Error>
+            auto operator()(Error const& current_error) {
+                auto const delta_error = current_error - m_previousError;
+                m_previousError = current_error;
+                return m_factor * delta_error / m_interval;
+            }
 
-            private:
-                Kd const m_factor;
-                TimeInterval const m_interval;
-                PreviousError m_previousError;
-        };
-    }
+        private:
+            Kd const m_factor;
+            TimeInterval const m_interval;
+            PreviousError m_previousError;
+    };
 }
 
 #endif
